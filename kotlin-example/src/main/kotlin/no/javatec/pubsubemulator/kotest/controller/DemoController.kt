a69a8a5d01094850bbe92026cf2c6aa9e4d7aee7
Roar Skinderviken <roar.skinderviken@gmail.com>
2024-04-29 20:26:22 +0200

    Added @ExecuteOn(TaskExecutors.IO) on controllers

--- kotlin-example/src/main/kotlin/no/javatec/pubsubemulator/kotest/controller/DemoController.kt
+++ kotlin-example/src/main/kotlin/no/javatec/pubsubemulator/kotest/controller/DemoController.kt
@@ -2,9 +2,12 @@ package no.javatec.pubsubemulator.kotest.controller
 
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.*
+import io.micronaut.scheduling.TaskExecutors
+import io.micronaut.scheduling.annotation.ExecuteOn
 import no.javatec.pubsubemulator.kotest.dto.SampleInputMessage
 import no.javatec.pubsubemulator.kotest.service.ControllerService
 
+@ExecuteOn(TaskExecutors.IO)
 @Controller("/pubsubEmulatorDemo")
 class DemoController(private val demoService: ControllerService) {
 
